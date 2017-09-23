14a52cf5703084d4babd0e7a4c6de98ab1560c79
Charles <lcq@holdskill.com>
2017-09-23 15:31:52 +0800

    第九次发布
    第九次发布。

---
+++ src/android/PicassoImageLoader.java
@@ -0,0 +1,32 @@
+package com.holdskill.imagepicker;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.widget.ImageView;
+
+import com.your.package.name.R;
+import com.holdskill.imagepicker.loader.ImageLoader;
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+
+public class PicassoImageLoader implements ImageLoader {
+
+
+    @Override
+    public void displayImage(Activity activity, String path, ImageView imageView, int width, int height) {
+        Picasso.with(activity)//
+                .load(Uri.fromFile(new File(path)))//
+                .placeholder(R.mipmap.default_image)//
+                .error(R.mipmap.default_image)//
+                .resize(width, height)//
+                .centerInside()//
+                .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)//
+                .into(imageView);
+    }
+
+    @Override
+    public void clearMemoryCache() {
+    }
+}
---
+++ src/android/module/DataHolder.java
@@ -0,0 +1,50 @@
+package com.holdskill.imagepicker;
+
+import com.holdskill.imagepicker.bean.ImageItem;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * 新的DataHolder，使用单例和弱引用解决崩溃问题
+ * <p>
+ * Author: nanchen
+ * Email: liushilin520@foxmail.com
+ * Date: 2017-03-20  07:01
+ */
+public class DataHolder {
+    public static final String DH_CURRENT_IMAGE_FOLDER_ITEMS = "dh_current_image_folder_items";
+
+    private static com.holdskill.imagepicker.DataHolder mInstance;
+    private Map<String, List<ImageItem>> data;
+
+    public static com.holdskill.imagepicker.DataHolder getInstance() {
+        if (mInstance == null){
+            synchronized (com.holdskill.imagepicker.DataHolder.class){
+                if (mInstance == null){
+                    mInstance = new com.holdskill.imagepicker.DataHolder();
+                }
+            }
+        }
+        return mInstance;
+    }
+
+    private DataHolder() {
+        data = new HashMap<>();
+    }
+
+    public void save(String id, List<ImageItem> object) {
+        if (data != null){
+            data.put(id, object);
+        }
+    }
+
+    public Object retrieve(String id) {
+        if (data == null || mInstance == null){
+            throw new RuntimeException("你必须先初始化");
+        }
+        return data.get(id);
+    }
+}
